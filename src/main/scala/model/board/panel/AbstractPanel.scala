708fad1653db675e8b4e621195eeb7aaea90bc69
Zelcris <xzelcris@gmail.com>
2023-09-26 03:41:06 -0300

    Update AbstractPanel.scala
    add and remove where implemented

--- src/main/scala/model/board/panel/AbstractPanel.scala
+++ src/main/scala/model/board/panel/AbstractPanel.scala
@@ -1,5 +1,23 @@
 package cl.uchile.dcc.citric
 package model.board.panel
 
-abstract class AbstractPanel extends Panel {
+import model.character.PlayerCharacter
+import scala.collection.mutable.ArrayBuffer
+
+abstract class AbstractPanel(override val characters: ArrayBuffer[PlayerCharacter],
+                             override var nextPanels: ArrayBuffer[Panel])
+  extends Panel {
+  /** Adds a character to the list of characters currently on this panel.
+   *
+   * This might be invoked when a player moves to this panel or starts their turn on it.
+   *
+   * @param player The player character to add to this panel.
+   */
+  override def addCharacter(player: PlayerCharacter): Unit = {
+    characters += player
+  }
+
+  override def removeCharacter(player: PlayerCharacter): Unit = {
+    characters -= player
+  }
 }
