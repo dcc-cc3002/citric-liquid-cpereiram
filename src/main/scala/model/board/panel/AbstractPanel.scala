5a1db629ffb6c26ba3e7efbaa396ac8cc7a157fd
cpereiram <122063130+cpereiram@users.noreply.github.com>
2023-09-26 15:35:34 -0300

    Update AbstractPanel.scala

--- src/main/scala/model/board/panel/AbstractPanel.scala
+++ src/main/scala/model/board/panel/AbstractPanel.scala
@@ -1,23 +1,41 @@
 package cl.uchile.dcc.citric
 package model.board.panel
 
-import model.character.PlayerCharacter
+import model.character.Character
 import scala.collection.mutable.ArrayBuffer
 
-abstract class AbstractPanel(override val characters: ArrayBuffer[PlayerCharacter],
-                             override var nextPanels: ArrayBuffer[Panel])
+/** An abstract class representing a Panel.
+ *
+ * Every Panel are represented by:
+ *
+ * @param characters List of characters currently on this panel. No one could be repeated.
+ * @param nextPanels List of panels next to this current panel.
+ *
+ * @constructor Create a new Panel.
+ * @author [[https://github.com/zelcris Cristóbal Pereira M.]]
+ * @version 1.1
+ * @since 1.0
+ */
+abstract class AbstractPanel(val characters: ArrayBuffer[Character],
+                             val nextPanels: ArrayBuffer[Panel])
   extends Panel {
-  /** Adds a character to the list of characters currently on this panel.
-   *
-   * This might be invoked when a player moves to this panel or starts their turn on it.
-   *
-   * @param player The player character to add to this panel.
-   */
-  override def addCharacter(player: PlayerCharacter): Unit = {
+  /// Documentation inherited from [[Panel]]
+  override def addCharacter(player: Character): Unit = {
     characters += player
   }
 
-  override def removeCharacter(player: PlayerCharacter): Unit = {
+  /// Documentation inherited from [[Panel]]
+  override def removeCharacter(player: Character): Unit = {
     characters -= player
+  }
+
+  /// Documentation inherited from [[Panel]]
+  override def addNextPanel(panel: Panel): Unit = {
+    nextPanels += panel
+  }
+
+  /// Documentation inherited from [[Panel]]
+  override def removeNextPanel(panel: Panel): Unit = {
+    nextPanels -= panel
   }
 }
