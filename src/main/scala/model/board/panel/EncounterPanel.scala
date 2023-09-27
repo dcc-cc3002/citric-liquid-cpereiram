40137f406eca631bccd3e5a22450046d890cd3e5
cpereiram <122063130+cpereiram@users.noreply.github.com>
2023-09-26 23:37:07 -0300

    Update EncounterPanel.scala
    Equals added

--- src/main/scala/model/board/panel/EncounterPanel.scala
+++ src/main/scala/model/board/panel/EncounterPanel.scala
@@ -1,38 +1,31 @@
 package cl.uchile.dcc.citric
 package model.board.panel
 
-import cl.uchile.dcc.citric.model.character.PlayerCharacter
+import model.character.Character
+
+import java.util.Objects
 import scala.collection.mutable.ArrayBuffer
 
-class EncounterPanel extends AbstractPanel{
-  /** Array of the characters currently positioned on this panel.
-   *
-   * In the game, multiple characters might be on the same panel at once, e.g., if multiple players
-   * land on the same space.
-   */
-  override val characters: ArrayBuffer[PlayerCharacter] = ???
-  /** An array of panels that are directly connected to this one.
-   *
-   * In the context of the game, multiple routes or paths may exist, this could represent the
-   * possible next steps a player might take after being on this panel.
-   *
-   * @return a List of Panel instances that are adjacent or connected to this panel.
-   */
-  override var nextPanels: ArrayBuffer[Panel] = ???
+class EncounterPanel(characters: ArrayBuffer[Character],
+                     nextPanels: ArrayBuffer[Panel])
+  extends AbstractPanel(characters, nextPanels) with Equals {
 
-  /** Adds a character to the list of characters currently on this panel.
-   *
-   * This might be invoked when a player moves to this panel or starts their turn on it.
-   *
-   * @param player The player character to add to this panel.
-   */
-  override def addCharacter(player: PlayerCharacter): Unit = ???
+  /// Documentation inherited from [[Equals]]
+  override def canEqual(that: Any): Boolean = that.isInstanceOf[EncounterPanel]
 
-  /** Removes a character from the list of characters currently on this panel.
-   *
-   * This might be invoked when a player moves off this panel.
-   *
-   * @param player The player character to remove from this panel.
-   */
-  override def removeCharacter(player: PlayerCharacter): Unit = ???
+  /// Documentation inherited from [[Equals]]
+  override def equals(that: Any): Boolean = {
+    if (canEqual(that)) {
+      val other = that.asInstanceOf[EncounterPanel]
+      characters.equals(other.characters) &&
+        nextPanels.equals(other.nextPanels)
+    } else {
+      false
+    }
+  }
+
+  /// Documentation inherited from [[Any]]
+  override def hashCode: Int = {
+    Objects.hash(classOf[EncounterPanel], characters, nextPanels)
+  }
 }
