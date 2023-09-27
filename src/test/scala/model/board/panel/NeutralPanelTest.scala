d727b6f5aac6ed87ffd8261aa6f2133e0e02c6b5
cpereiram <122063130+cpereiram@users.noreply.github.com>
2023-09-26 23:38:25 -0300

    Update NeutralPanelTest.scala
    New test lines added to improve coverage

--- src/test/scala/model/board/panel/NeutralPanelTest.scala
+++ src/test/scala/model/board/panel/NeutralPanelTest.scala
@@ -1,40 +1,79 @@
 package cl.uchile.dcc.citric
 package model.board.panel
 
+import model.character.Character
 import model.character.PlayerCharacter
 
+import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
 class NeutralPanelTest extends munit.FunSuite {
   /* Current values of testPlayer. */
-  private val name = "testPlayer"
+  private val name1 = "testPlayer1"
+  private val name2 = "testPlayer2"
   private val maxHp = 10
+  private val hp = 5
   private val attack = 1
   private val defense = 1
   private val evasion = 1
+  private val ko = false
   private val randomNumberGenerator = new Random(11)
 
-  /* Pre-definition of the player. */
-  private var character: PlayerCharacter = _
-  private var panel: NeutralPanel = _
+  /* Current values of testPanel. */
+  private val characters1 = ArrayBuffer[Character]()
+  private val characters2 = ArrayBuffer[Character]()
+  private val nextPanels1 = ArrayBuffer[Panel]()
+  private val nextPanels2 = ArrayBuffer[Panel]()
+
+  /* Pre-definition of the characters and panels. */
+  private var character1: PlayerCharacter = _
+  private var character2: PlayerCharacter = _
+  private var panel1: NeutralPanel = _
+  private var panel2: NeutralPanel = _
+  nextPanels1 += panel1
+  nextPanels2 += panel2
+
 
   override def beforeEach(context: BeforeEach): Unit = {
-    character = new PlayerCharacter(
-      name,
+    character1 = new PlayerCharacter(
+      name1,
       maxHp,
+      hp,
       attack,
       defense,
       evasion,
-      randomNumberGenerator
-    )
+      ko,
+      randomNumberGenerator)
+    character2 = new PlayerCharacter(
+      name2,
+      maxHp,
+      hp,
+      attack,
+      defense,
+      evasion,
+      ko,
+      randomNumberGenerator)
+    panel1 = new NeutralPanel(characters1, nextPanels1)
+    panel2 = new NeutralPanel(characters2, nextPanels2)
+  }
+
+  test("A NeutralPanel has params") {
+    assertEquals(panel1.characters, characters1)
+    assertEquals(panel1.nextPanels, nextPanels1)
+    assertEquals(panel2.characters, characters2)
+    assertEquals(panel2.nextPanels, nextPanels2)
   }
 
   test("A panel can be equal to another one.") {
-
+    assertEquals(panel1, panel1)
+    assertEquals(panel1, new NeutralPanel(characters1, nextPanels1))
+    panel1.addCharacter(character1)
+    assertNotEquals(panel1, panel2)
+    assert(!panel1.equals(character1))
   }
 
   test("The hash code of a NeutralPanel is consistent with equals") {
-    //assertEquals(new FireBakemon(name, hp).##, fireBakemon.##)
+    assertEquals(new NeutralPanel(characters1, nextPanels1).##, panel1.##)
   }
 
   test("A player can be in the panel.") {
@@ -46,7 +85,12 @@ class NeutralPanelTest extends munit.FunSuite {
   }
 
   test("A player can leave the panel.") {
-
+    panel1.addCharacter(character1)
+    panel1.addCharacter(character2)
+    panel1.characters(0).equals(character1)
+    panel1.characters(1).equals(character2)
+    panel1.removeCharacter(character1)
+    panel1.characters(0).equals(character2)
   }
 
   test("A player can be in the panel.") {
