6846d8631f6585d618aa00d5e67f650706ea91b7
Zelcris <xzelcris@gmail.com>
2023-10-12 13:16:44 -0300

    Documentation added.

--- src/main/scala/model/Norma.scala
+++ src/main/scala/model/Norma.scala
@@ -1,6 +1,17 @@
 package cl.uchile.dcc.citric
 package model
 
+/** Represents a Norma value, fundamental field for a Character.
+ *
+ * Each Norma has its own parameters and they work as levels for a Character.
+ *
+ * All the normas should be checked.
+ *
+ * @author [[https://github.com/r8vnhill Ignacio Slater M.]]
+ * @author [[https://github.com/zelcris Cristóbal Pereira M.]]
+ * @version 1.1
+ * @since 1.0
+ */
 trait Norma {
 
 }
