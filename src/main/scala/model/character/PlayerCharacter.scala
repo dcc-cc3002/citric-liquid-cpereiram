70355d6184105444ba182d7695cfe0b9368a75ef
Zelcris <xzelcris@gmail.com>
2023-10-15 23:49:27 -0300

    Update PlayerCharacter.scala

--- src/main/scala/model/character/PlayerCharacter.scala
+++ src/main/scala/model/character/PlayerCharacter.scala
@@ -5,61 +5,70 @@ import cl.uchile.dcc.citric.model.norma.Norma
 import scala.util.Random
 
 /** The `PlayerCharacter` class represents a character or avatar in the game, encapsulating
-  * several attributes such as health points, attack strength, defense capability,
-  * and evasion skills. Each player has a unique name, and throughout the game,
-  * players can collect stars, roll dice, and progress in levels known as 'norma'.
-  * This class not only maintains the state of a player but also provides methods
-  * to modify and interact with these attributes.
-  *
-  * For instance, players can:
+ * several attributes such as health points, attack strength, defense capability,
+ * and evasion skills. Each player has a unique name, and throughout the game,
+ * players can collect stars, roll dice, and progress in levels known as 'norma'.
+ * This class not only maintains the state of a player but also provides methods
+ * to modify and interact with these attributes.
  *
-  * - Increase or decrease their star count.
+ * For instance, players can:
  *
-  * - Roll a dice, a common action in many board games.
+ * - Increase or decrease their star count.
  *
-  * - Advance their norma level.
-  *
-  * Furthermore, the `Player` class has a utility for generating random numbers,
-  * which is primarily used for simulating dice rolls. By default, this utility is
-  * an instance of the `Random` class but can be replaced if different random
-  * generation behaviors are desired.
-  *
-  * @param name The name of the player. This is an identifier and should be unique.
-  * @param maxHp The maximum health points a player can have. It represents the player's endurance.
-  * @param hp The current health points of a character.
-  * @param attack The player's capability to deal damage to opponents.
-  * @param defense The player's capability to resist or mitigate damage from opponents.
-  * @param evasion The player's skill to completely avoid certain attacks.
-  * @param ko Indicates if a character is Knockout.
-  * @param randomNumberGenerator A utility to generate random numbers. Defaults to a new `Random`
-  *                              instance.
-  *
-  * @author [[https://github.com/danielRamirezL/ Daniel Ramírez L.]]
-  * @author [[https://github.com/joelriquelme/ Joel Riquelme P.]]
-  * @author [[https://github.com/r8vnhill/ Ignacio Slater M.]]
-  * @author [[https://github.com/Seivier/ Vicente González B.]]
-  * @author [[https://github.com/zelcris Cristóbal Pereira M.]]
-  */
+ * - Roll a dice, a common action in many board games.
+ *
+ * - Advance their norma level.
+ *
+ * Furthermore, the `Player` class has a utility for generating random numbers,
+ * which is primarily used for simulating dice rolls. By default, this utility is
+ * an instance of the `Random` class but can be replaced if different random
+ * generation behaviors are desired.
+ *
+ * @param name The name of the player. This is an identifier and should be unique.
+ * @param maxHp The maximum health points a player can have. It represents the player's endurance.
+ * @param hp The current health points of a character.
+ * @param attack The player's capability to deal damage to opponents.
+ * @param defense The player's capability to resist or mitigate damage from opponents.
+ * @param evasion The player's skill to completely avoid certain attacks.
+ * @param norma The current norma of the character.
+ * @param stars The current stars a character have.
+ * @param victories The current victories a character have.
+ * @param randomNumberGenerator A utility to generate random numbers. Defaults to a new `Random`
+ *                              instance.
+ *
+ * @author [[https://github.com/danielRamirezL/ Daniel Ramírez L.]]
+ * @author [[https://github.com/joelriquelme/ Joel Riquelme P.]]
+ * @author [[https://github.com/r8vnhill/ Ignacio Slater M.]]
+ * @author [[https://github.com/Seivier/ Vicente González B.]]
+ * @author [[https://github.com/zelcris Cristóbal Pereira M.]]
+ */
 class PlayerCharacter( name: String,
                        maxHp: Int,
                        hp: Int,
                        attack: Int,
                        defense: Int,
                        evasion: Int,
-                       ko: Boolean,
-                       norma: Norma,
-                       val randomNumberGenerator: Random = new Random())
+                       protected val _norma: Norma,
+                       protected val _stars: Int,
+                       protected val _victories: Int,
+                       protected val randomNumberGenerator: Random = new Random())
   extends AbstractCharacter(name,
                             maxHp,
                             hp,
                             attack,
                             defense,
-                            evasion,
-                            ko,
-                            norma) {
+                            evasion) {
+
+  /** The norma of a Character. */
+  protected var _norma: Norma
 
   /** Rolls a dice and returns a value between 1 to 6. */
   def rollDice(): Int = {
     randomNumberGenerator.nextInt(6) + 1
   }
 }
+
+/** The norma of a Character. */
+protected var _norma: Norma
+/** Returns the current norma of the character. */
+def norma: Norma = _norma
