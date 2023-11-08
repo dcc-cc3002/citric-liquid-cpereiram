39c797de1d3ea44557e6b6306aed4093b765c754
Zelcris <xzelcris@gmail.com>
2023-11-08 19:45:29 -0300

    Update PlayerCharacterTest.scala

--- src/test/scala/model/character/PlayerCharacterTest.scala
+++ src/test/scala/model/character/PlayerCharacterTest.scala
@@ -2,6 +2,8 @@ package cl.uchile.dcc.citric
 package model.character
 
 import cl.uchile.dcc.citric.model.norma.Norma1
+
+import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
 class PlayerCharacterTest extends munit.FunSuite {
@@ -11,8 +13,9 @@ class PlayerCharacterTest extends munit.FunSuite {
   This will make your tests more readable, easier to maintain, and less error-prone.
   */
   private val name = "testPlayer"
+  private val name2 = "testPlayer2"
   private val maxHp = 10
-  private var hp = 5
+  private val hp = 5
   private val attack = 1
   private val defense = 1
   private val evasion = 1
@@ -29,12 +32,24 @@ class PlayerCharacterTest extends munit.FunSuite {
   to worry about the state of the object between tests.
   */
   private var character: PlayerCharacter = _  // <- x = _ is the same as x = null
+  private var character2: PlayerCharacter = _
   /* Add any other variables you need here... */
 
   // This method is executed before each `test(...)` method.
   override def beforeEach(context: BeforeEach): Unit = {
     character = new PlayerCharacter(
       name,
+      maxHp,
+      hp,
+      attack,
+      defense,
+      evasion,
+      norma,
+      stars,
+      victories,
+      randomNumberGenerator)
+    character2 = new PlayerCharacter(
+      name2,
       maxHp,
       hp,
       attack,
@@ -66,5 +81,42 @@ class PlayerCharacterTest extends munit.FunSuite {
 
   test("A PlayerCharacter could be equal to another with the same attributes values.") {
     assertEquals(new PlayerCharacter(name, maxHp, hp, attack, defense, evasion, norma, stars, victories, new Random(11)), character)
+    assertEquals(character, character)
+    assertNotEquals(character, character2)
+    assert(!character.equals(name))
+  }
+
+  test("A PlayerCharacter stats can be set") {
+    character.hp_(10)
+    assertEquals(character.hp, 10)
+    character.attack_(10)
+    assertEquals(character.attack, 10)
+    character.defense_(10)
+    assertEquals(character.defense, 10)
+    character.evasion_(10)
+    assertEquals(character.evasion, 10)
+    character.norma_(new Norma1(false))
+    assertEquals(character.norma, new Norma1(false))
+    character.stars_(10)
+    assertEquals(character.stars, 10)
+    character.victories_(10)
+    assertEquals(character.victories, 10)
+  }
+
+  test("A Character can be KO") {
+    character.hp_(0)
+    assert(character.isKo())
+    character.hp_(20)
+    assert(!character.isKo())
+  }
+
+  test("A Player can do damage to another Character") {
+    character.doDamage(character2, 1)
+    assertEquals(character2.hp, 4)
+    character.doDamage(character2, 4)
+    assertEquals(character2.hp, 0)
+    character.doDamage(character2, 1)
+    //Should be an exception
+    assertEquals(character2.hp, 0)
   }
 }
