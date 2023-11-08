4f5e6503f0e4a5e4905e0ea49622ab0c40ff0b94
Zelcris <xzelcris@gmail.com>
2023-11-08 19:44:21 -0300

    Wild Characters added

---
+++ src/main/scala/model/character/wild/Chicken.scala
@@ -0,0 +1,50 @@
+package cl.uchile.dcc.citric
+package model.character.wild
+
+import java.util.Objects
+
+class Chicken(name: String,
+              maxHp: Int = 3,
+              stars: Int = 0)
+  extends AbstractWildCharacter(name,
+    maxHp,
+    stars) with Equals {
+
+  override protected var _hp: Int = 3
+  override protected var _attack: Int = -1
+  override protected var _defense: Int = -1
+  override protected var _evasion: Int = 1
+
+  //EQUALS AND HASH METHODS
+  /// Documentation inherited from [[Equals]]
+  override def canEqual(that: Any): Boolean = that.isInstanceOf[Chicken]
+
+  /// Documentation inherited from [[Equals]]
+  override def equals(that: Any): Boolean = {
+    if (canEqual(that)) {
+      val other = that.asInstanceOf[Chicken]
+      name == other.name &&
+        maxHp == other.maxHp &&
+        hp == other.hp &&
+        attack == other.attack &&
+        defense == other.defense &&
+        evasion == other.evasion &&
+        stars == other.stars
+    } else {
+      false
+    }
+  }
+
+  /// Documentation inherited from [[Any]]
+  override def hashCode: Int = {
+    Objects.hash(classOf[Chicken],
+      name,
+      maxHp,
+      hp,
+      attack,
+      defense,
+      evasion,
+      stars)
+  }
+
+}
