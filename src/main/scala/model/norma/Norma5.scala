f98c407da1ef6b6ac61647ef9eddea5206f46c96
Zelcris <xzelcris@gmail.com>
2023-11-08 19:42:41 -0300

    Create Norma5.scala

---
+++ src/main/scala/model/norma/Norma5.scala
@@ -0,0 +1,56 @@
+package cl.uchile.dcc.citric
+package model.norma
+
+import model.character.PlayerCharacter
+
+import java.util.Objects
+
+/** A norma that requires 200 stars or 14 victories to be achieved, depending on the mode.
+ *
+ * @param starMode Whether the norma is star-based or victory-based.
+ *
+ * @author [[https://github.com/zelcris Cristóbal Pereira M.]]
+ * @version 1.1
+ * @since 1.0
+ */
+class Norma5(starMode: Boolean) extends AbstractNorma(starMode: Boolean) with Equals {
+  //FIELDS
+  override protected val _starsRequired: Int = 200
+  override protected val _victoriesRequired: Int = 14
+  override protected var _nextNorma: Norma5 = new Norma5(starMode)
+
+
+  //GETTERS
+  /// Documentation inherited from [[Norma]]
+  override def nextNorma: Norma5 = _nextNorma
+
+
+  //SETTERS
+  /// Documentation inherited from [[Norma]]
+  def nextNorma_(newNorma: Norma5): Unit = {
+    _nextNorma = newNorma
+  }
+
+
+  //EQUALS METHODS
+  /// Documentation inherited from [[Equals]]
+  override def canEqual(that: Any): Boolean = that.isInstanceOf[Norma5]
+
+  /// Documentation inherited from [[Equals]]
+  override def equals(that: Any): Boolean = {
+    if (canEqual(that)) {
+      val other = that.asInstanceOf[Norma5]
+      starMode == other.starMode &&
+        starsRequired == other.starsRequired &&
+        victoriesRequired == other.victoriesRequired
+    } else {
+      false
+    }
+  }
+
+  /// Documentation inherited from [[Any]]
+  override def hashCode: Int = {
+    Objects.hash(classOf[Norma5],
+      starMode)
+  }
+}
