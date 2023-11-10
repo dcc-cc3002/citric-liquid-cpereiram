819eb6a4a1edf1b70d0153c7d3dbafb05d044cbd
Zelcris <xzelcris@gmail.com>
2023-11-09 21:03:23 -0300

    WildCharacter tests update

--- src/test/scala/model/character/ChickenTest.scala
+++ src/test/scala/model/character/ChickenTest.scala
@@ -1,7 +1,11 @@
 package cl.uchile.dcc.citric
 package model.character
 
-import model.character.wild.Chicken
+import model.character.wild.{Chicken, RoboBall, Seagull}
+
+import cl.uchile.dcc.citric.model.norma.Norma1
+
+import scala.util.Random
 
 class ChickenTest extends munit.FunSuite {
   private val name = "Chicken"
@@ -11,14 +15,33 @@ class ChickenTest extends munit.FunSuite {
   private val attack = -1
   private val defense = -1
   private val evasion = 1
+  private val norma = new Norma1(true)
   private val stars = 0
-  
+  private val victories = 0
+  private val randomNumberGenerator = new Random(11)
+
+
   private var chicken: Chicken = _
   private var chicken2: Chicken = _
+  private var roboball: RoboBall = _
+  private var seagull: Seagull = _
+  private var player: PlayerCharacter = _
   
   override def beforeEach(context: BeforeEach): Unit = {
-    chicken = new Chicken(name)
-    chicken2 = new Chicken(name2)
+    chicken = new Chicken(name, randomNumberGenerator = randomNumberGenerator)
+    chicken2 = new Chicken(name2, randomNumberGenerator = randomNumberGenerator)
+    roboball = new RoboBall("1", randomNumberGenerator = randomNumberGenerator)
+    seagull = new Seagull("2", randomNumberGenerator = randomNumberGenerator)
+    player = new PlayerCharacter("Player",
+      maxHp,
+      1,
+      attack,
+      defense,
+      evasion,
+      norma,
+      stars,
+      victories,
+      randomNumberGenerator)
   }
   
   test("A Chicken should have correctly set their attributes") {
@@ -33,14 +56,14 @@ class ChickenTest extends munit.FunSuite {
 
   test("A Chicken could be equal to another Chicken") {
     assertEquals(chicken, chicken)
-    assertEquals(chicken, new Chicken(name))
+    assertEquals(chicken, new Chicken(name, randomNumberGenerator = randomNumberGenerator))
     assertNotEquals(chicken, chicken2)
     assert(!chicken.equals(name))
   }
 
   test("The hash code of a Chicken is consistent with equals") {
-    assertEquals(new Chicken(name).##, chicken.##)
-    assertEquals(new Chicken(name2).##, chicken2.##)
+    assertEquals(new Chicken(name, randomNumberGenerator = randomNumberGenerator).##, chicken.##)
+    assertEquals(new Chicken(name2, randomNumberGenerator = randomNumberGenerator).##, chicken2.##)
   }
 
   test("A Chicken stats can be set") {
@@ -59,5 +82,29 @@ class ChickenTest extends munit.FunSuite {
     assert(chicken.isKo())
     chicken.hp_(1)
     assert(!chicken.isKo())
+  }
+
+  test("A Seagull can attack a WildCharacter and nothing happens.") {
+    chicken.toAttack(chicken2, true)
+    assertEquals(chicken2.hp, 3)
+    chicken.toAttack(chicken2, false)
+    assertEquals(chicken2.hp, 3)
+    chicken.toAttack(roboball, true)
+    assertEquals(roboball.hp, 3)
+    chicken.toAttack(roboball, false)
+    assertEquals(roboball.hp, 3)
+    chicken.toAttack(seagull, true)
+    assertEquals(seagull.hp, 3)
+    chicken.toAttack(seagull, false)
+    assertEquals(seagull.hp, 3)
+  }
+
+  test("A Chicken can attack a PlayerCharacter.") {
+    //if player defends, he will die.
+    chicken.toAttack(player, true)
+    //assert(player.isKo())
+    //if player evades, he could die (random).
+    chicken.toAttack(player, false)
+    assert(player.hp <= 1)
   }
 }
--- src/test/scala/model/character/SeagullTest.scala
+++ src/test/scala/model/character/SeagullTest.scala
@@ -1,7 +1,11 @@
 package cl.uchile.dcc.citric
 package model.character
 
-import model.character.wild.Seagull
+import model.character.wild.{Chicken, RoboBall, Seagull}
+
+import cl.uchile.dcc.citric.model.norma.Norma1
+
+import scala.util.Random
 
 class SeagullTest extends munit.FunSuite {
   private val name = "Seagull"
@@ -11,14 +15,33 @@ class SeagullTest extends munit.FunSuite {
   private val attack = 1
   private val defense = -1
   private val evasion = -1
+  private val norma = new Norma1(true)
   private val stars = 0
+  private val victories = 0
+  private val randomNumberGenerator = new Random(11)
+
 
   private var seagull: Seagull = _
   private var seagull2: Seagull = _
+  private var roboball: RoboBall = _
+  private var chicken: Chicken = _
+  private var player: PlayerCharacter = _
 
   override def beforeEach(context: BeforeEach): Unit = {
-    seagull = new Seagull(name)
-    seagull2 = new Seagull(name2)
+    seagull = new Seagull(name, randomNumberGenerator = randomNumberGenerator)
+    seagull2 = new Seagull(name2, randomNumberGenerator = randomNumberGenerator)
+    roboball = new RoboBall("1", randomNumberGenerator = randomNumberGenerator)
+    chicken = new Chicken("2", randomNumberGenerator = randomNumberGenerator)
+    player = new PlayerCharacter("Player",
+      maxHp,
+      1,
+      attack,
+      defense,
+      evasion,
+      norma,
+      stars,
+      victories,
+      randomNumberGenerator)
   }
 
   test("A Seagull should have correctly set their attributes") {
@@ -33,14 +56,14 @@ class SeagullTest extends munit.FunSuite {
 
   test("A Seagull could be equal to another Seagull") {
     assertEquals(seagull, seagull)
-    assertEquals(seagull, new Seagull(name))
+    assertEquals(seagull, new Seagull(name, randomNumberGenerator = randomNumberGenerator))
     assertNotEquals(seagull, seagull2)
     assert(!seagull.equals(name))
   }
 
   test("The hash code of a Seagull is consistent with equals") {
-    assertEquals(new Seagull(name).##, seagull.##)
-    assertEquals(new Seagull(name2).##, seagull2.##)
+    assertEquals(new Seagull(name, randomNumberGenerator = randomNumberGenerator).##, seagull.##)
+    assertEquals(new Seagull(name2, randomNumberGenerator = randomNumberGenerator).##, seagull2.##)
   }
 
   test("A Seagull stats can be set") {
@@ -59,5 +82,29 @@ class SeagullTest extends munit.FunSuite {
     assert(seagull.isKo())
     seagull.hp_(1)
     assert(!seagull.isKo())
+  }
+
+  test("A Seagull can attack a WildCharacter and nothing happens.") {
+    seagull.toAttack(seagull2, true)
+    assertEquals(seagull2.hp, 3)
+    seagull.toAttack(seagull2, false)
+    assertEquals(seagull2.hp, 3)
+    seagull.toAttack(roboball, true)
+    assertEquals(roboball.hp, 3)
+    seagull.toAttack(roboball, false)
+    assertEquals(roboball.hp, 3)
+    seagull.toAttack(chicken, true)
+    assertEquals(chicken.hp, 3)
+    seagull.toAttack(chicken, false)
+    assertEquals(chicken.hp, 3)
+  }
+
+  test("A Seagull can attack a PlayerCharacter.") {
+    //if player defends, he will die.
+    seagull.toAttack(player, true)
+    //assert(player.isKo())
+    //if player evades, he could die (random).
+    seagull.toAttack(player, false)
+    assert(player.hp <= 1)
   }
 }
