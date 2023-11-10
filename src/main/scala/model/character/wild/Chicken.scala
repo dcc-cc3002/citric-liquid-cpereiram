d82f4694d77db080f6268a64e468f93dd02b6d8c
Zelcris <xzelcris@gmail.com>
2023-11-09 21:02:34 -0300

    Update Chicken.scala
    Double dispatch added

--- src/main/scala/model/character/wild/Chicken.scala
+++ src/main/scala/model/character/wild/Chicken.scala
@@ -1,14 +1,14 @@
 package cl.uchile.dcc.citric
 package model.character.wild
 
-import java.util.Objects
+import cl.uchile.dcc.citric.model.character.PlayerCharacter
+import cl.uchile.dcc.citric.model.character.Character
 
-class Chicken(name: String,
-              maxHp: Int = 3,
-              stars: Int = 0)
-  extends AbstractWildCharacter(name,
-    maxHp,
-    stars) with Equals {
+import java.util.Objects
+import scala.util.Random
+
+class Chicken(name: String, maxHp: Int = 3, stars: Int = 0, randomNumberGenerator: Random)
+  extends AbstractWildCharacter(name, maxHp, stars, randomNumberGenerator) with Equals {
 
   override protected var _hp: Int = 3
   override protected var _attack: Int = -1
@@ -46,5 +46,101 @@ class Chicken(name: String,
       evasion,
       stars)
   }
+
+  /// Documentation inherited from [[Character]]
+  def toAttack(other: Character, choice: Boolean): Unit = {
+    other.toAttackByChicken(this, choice)
+  }
+
+  /// Documentation inherited from [[Character]]
+  def toAttackByPlayer(other: PlayerCharacter, choice: Boolean): Unit = {
+    if (choice) {
+      this.toDefend(other)
+    } else {
+      this.toEvade(other)
+    }
+  }
+
+  def toAttackByChicken(other: Chicken, choice: Boolean): Unit = {
+    if (choice) {
+      this.toDefend(other)
+    } else {
+      this.toEvade(other)
+    }
+  }
+
+  def toAttackBySeagull(other: Seagull, choice: Boolean): Unit = {
+    if (choice) {
+      this.toDefend(other)
+    } else {
+      this.toEvade(other)
+    }
+  }
+
+  def toAttackByRoboBall(other: RoboBall, choice: Boolean): Unit = {
+    if (choice) {
+      this.toDefend(other)
+    } else {
+      this.toEvade(other)
+    }
+  }
+
+  override def toDefend(other: Character): Unit = {
+    other.toDefendByChicken(this)
+  }
+
+  override def toDefendByPlayer(other: PlayerCharacter): Unit = {
+    val roll_defense = other.rollDice()
+    val roll_attack = this.rollDice()
+    val damage = 1.max(roll_attack + this.attack - roll_defense - other.defense)
+    other.hp_(other.hp - damage)
+    if (other.hp <= 0) {
+      other.stars_(other.stars / 2)
+      this.stars_(this.stars + other.stars)
+    }
+  }
+
+  override def toDefendByChicken(other: Chicken): Unit = {
+    // Nothing happens, they are alies.
+  }
+
+  override def toDefendBySeagull(other: Seagull): Unit = {
+    // Nothing happens, they are alies.
+  }
+
+  override def toDefendByRoboBall(other: RoboBall): Unit = {
+    // Nothing happens, they are alies.
+  }
+
+  override def toEvade(other: Character): Unit = {
+    other.toEvadeByChicken(this)
+  }
+
+
+  override def toEvadeByPlayer(other: PlayerCharacter): Unit = {
+    val roll_evasion = other.rollDice()
+    val roll_attack = this.rollDice()
+    val damage = roll_attack + this.attack
+    if (roll_evasion + other.evasion <= damage) {
+      other.hp_(other.hp - damage)
+    }
+    if (other.hp <= 0) {
+      other.stars_(other.stars / 2)
+      this.stars_(this.stars + other.stars)
+    }
+  }
+
+  override def toEvadeByChicken(other: Chicken): Unit = {
+    // Nothing happens, they are alies.
+  }
+
+  override def toEvadeBySeagull(other: Seagull): Unit = {
+    // Nothing happens, they are alies.
+  }
+
+  override def toEvadeByRoboBall(other: RoboBall): Unit = {
+    // Nothing happens, they are alies.
+  }
+
 
 }
