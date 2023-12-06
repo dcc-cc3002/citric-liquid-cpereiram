9a3e2f37b3ae5b5ea96ccb3ab07dbaa937df2558
Zelcris <xzelcris@gmail.com>
2023-12-05 23:35:17 -0300

    Update InvalidTransitionException.scala

--- src/main/scala/exceptions/InvalidTransitionException.scala
+++ src/main/scala/exceptions/InvalidTransitionException.scala
@@ -5,5 +5,5 @@ package exceptions
  * Custom exception to signal an invalid transition encountered.
  * @param details A descriptive message detailing the nature of the invalid transition.
  */
-class InvalidTransitionError(details: String) extends Exception(s"An invalid transition was executed -- $details") {
+class InvalidTransitionException(details: String) extends Exception(details) {
 }
